96fd809364e6e12a12b86dcd5564db6d1815ab1d
mikan <83708535+piny940@users.noreply.github.com>
2022-10-04 15:16:27 +0900

    vscodeで環境構築

--- src/ch/idsia/scenarios/Main.java
+++ src/ch/idsia/scenarios/Main.java
@@ -33,24 +33,23 @@ import ch.idsia.agents.Agent;
 import ch.idsia.agents.controllers.ForwardJumpingAgent;
 
 /**
- * Created by IntelliJ IDEA. User: Sergey Karakovskiy, sergey at idsia dot ch Date: Mar 17, 2010 Time: 8:28:00 AM
+ * Created by IntelliJ IDEA. User: Sergey Karakovskiy, sergey at idsia dot ch
+ * Date: Mar 17, 2010 Time: 8:28:00 AM
  * Package: ch.idsia.scenarios
  */
-public final class Main
-{
-public static void main(String[] args)
-{
-    final MarioAIOptions marioAIOptions = new MarioAIOptions(args);
-    final Agent agent = new ForwardJumpingAgent();
-    marioAIOptions.setAgent(agent);
-    
-    int seed = 99;
-    marioAIOptions.setLevelRandSeed(seed);
-    
-    final BasicTask basicTask = new BasicTask(marioAIOptions);
-    basicTask.setOptionsAndReset(marioAIOptions);
-    basicTask.doEpisodes(1,true,1);
-    System.exit(0);
-}
+public final class Main {
+    public static void main(String[] args) {
+        final MarioAIOptions marioAIOptions = new MarioAIOptions(args);
+        final Agent agent = new ForwardJumpingAgent();
+        marioAIOptions.setAgent(agent);
+
+        int seed = 99;
+        marioAIOptions.setLevelRandSeed(seed);
+
+        final BasicTask basicTask = new BasicTask(marioAIOptions);
+        basicTask.setOptionsAndReset(marioAIOptions);
+        basicTask.doEpisodes(1, true, 1);
+        System.exit(0);
+    }
 
 }
