d827654105a3afecf585a94c7a4addd4839d2bbe
mikan <83708535+piny940@users.noreply.github.com>
2022-10-14 20:40:10 +0900

    GAAgentを作成

--- src/ch/idsia/agents/GAAgent.java
+++ src/ch/idsia/agents/GAAgent.java
@@ -11,188 +11,191 @@ import ch.idsia.evolution.Evolvable;
  * xをyビットだけ左シフトする。空いた下位ビット(右側)には0を挿入。
  */
 public class GAAgent extends BasicMarioAIAgent
-implements Agent,Evolvable,Comparable,Cloneable{
+    implements Agent, Evolvable, Comparable, Cloneable {
 
-	static String name = "GAAgent";
+  static String name = "GAAgent";
 
-	/* 遺伝子情報 */
-	public byte[] gene;
+  /* 遺伝子情報 */
+  public byte[] gene;
 
-	/* 各個体の評価値保存用変数 */
-	public int fitness;
+  /* 各個体の評価値保存用変数 */
+  public int fitness;
 
-	/* 環境から取得する入力数 */
-	public int inputNum = 16;
+  /* 環境から取得する入力数 */
+  public int inputNum = 16;
 
-	/* 乱数用変数 r */
-	Random r = new Random();
+  /* 乱数用変数 r */
+  Random r = new Random();
 
-	/* コンストラクタ */
-	public GAAgent(){
+  /* コンストラクタ */
+  public GAAgent() {
 
-		super(name);
+    super(name);
 
-		/* 16ビットの入力なので，65536(=2^16)個用意する */
-		gene = new byte[(1 << inputNum)];
+    /* 16ビットの入力なので，65536(=2^16)個用意する */
+    gene = new byte[(1 << inputNum)];
 
-		/* 出力は32(=2^5)パターン */
-		int num = 1 << (Environment.numberOfKeys -1);
+    /* 出力は32(=2^5)パターン */
+    int num = 1 << (Environment.numberOfKeys - 1);
 
-		int random;
-		int flag = 1;
+    int random;
+    int flag = 1;
 
-		/* geneの初期値は乱数(0から31)で取得 */
-		for(int i=0; i<gene.length; i++){
-			if(flag == 1){
-			switch(random = r.nextInt(8)){
-				case 0:
-					gene[i] = 0; break;
-				case 1:
-					gene[i] = 2; break;
-				case 2:
-					gene[i] = 8; break;
-				case 3:
-					gene[i] = 10; break;
-				case 4:
-					gene[i] = 16; break;
-				case 5:
-					gene[i] = 18; break;
-				case 6:
-					gene[i] = 24; break;
-				case 7:
-					gene[i] = 26; break;
+    /* geneの初期値は乱数(0から31)で取得 */
+    for (int i = 0; i < gene.length; i++) {
+      if (flag == 1) {
+        switch (random = r.nextInt(8)) {
+          case 0:
+            gene[i] = 0;
+            break;
+          case 1:
+            gene[i] = 2;
+            break;
+          case 2:
+            gene[i] = 8;
+            break;
+          case 3:
+            gene[i] = 10;
+            break;
+          case 4:
+            gene[i] = 16;
+            break;
+          case 5:
+            gene[i] = 18;
+            break;
+          case 6:
+            gene[i] = 24;
+            break;
+          case 7:
+            gene[i] = 26;
+            break;
 
-			}
-			}else{
-				gene[i]=(byte)r.nextInt(num);
-			}
+        }
+      } else {
+        gene[i] = (byte) r.nextInt(num);
+      }
 
+      // gene[i] = (byte)r.nextInt(17);
+    }
 
-//			gene[i] = (byte)r.nextInt(17);
-		}
+    /* 評価値を0で初期化 */
+    fitness = 0;
 
-		/* 評価値を0で初期化 */
-		fitness = 0;
+  }
 
+  /* compfit()追加記述 */
 
-	}
+  int distance;
 
-	/* compfit()追加記述 */
+  public void setFitness(int fitness) {
+    this.fitness = fitness;
+  }
 
-	int distance;
+  public int getFitness() {
+    return fitness;
+  }
 
-	public void setFitness(int fitness){
-		this.fitness = fitness;
-	}
+  /* 降順にソート */
+  public int compareTo(Object obj) {
+    GAAgent otherUser = (GAAgent) obj;
+    return -(this.fitness - otherUser.getFitness());
+  }
 
-	public int getFitness(){
-		return fitness;
-	}
+  /* compFit()追加記述ここまで */
 
-	/* 降順にソート */
-	public int compareTo(Object obj){
-	   	GAAgent otherUser = (GAAgent) obj;
-    	return -(this.fitness - otherUser.getFitness());
-	}
+  public boolean[] getAction() {
 
-	/* compFit()追加記述ここまで */
+    int input = 0;
 
+    /*
+     * 環境情報から input を決定
+     * 上位ビットから周辺近傍の状態を格納していく
+     */
 
-	public boolean[] getAction(){
+    /* enemies情報(上位7桁) */
+    input += probe(-1, -1, enemies) * (1 << 15); // probe * 2^15
+    input += probe(0, -1, enemies) * (1 << 14);
+    input += probe(1, -1, enemies) * (1 << 13);
+    input += probe(-1, 0, enemies) * (1 << 12);
+    input += probe(1, 0, enemies) * (1 << 11);
+    input += probe(-1, 1, enemies) * (1 << 10);
+    input += probe(1, 1, enemies) * (1 << 9);
 
-		int input = 0;
+    /* levelScene情報 */
+    input += probe(-1, -1, levelScene) * (1 << 8);
+    input += probe(0, -1, levelScene) * (1 << 7);
+    input += probe(1, -1, levelScene) * (1 << 6);
+    input += probe(-1, 0, levelScene) * (1 << 5);
+    input += probe(1, 0, levelScene) * (1 << 4);
+    input += probe(-1, 1, levelScene) * (1 << 3);
+    input += probe(1, 1, levelScene) * (1 << 2);
 
-		/* 環境情報から input を決定
-		 * 上位ビットから周辺近傍の状態を格納していく
-		 */
+    input += (isMarioOnGround ? 1 : 0) * (1 << 1);
+    input += (isMarioAbleToJump ? 1 : 0) * (1 << 0);
 
-		/* enemies情報(上位7桁) */
-		input += probe(-1,-1,enemies) * (1 << 15); //probe * 2^15
-		input += probe(0 ,-1,enemies) * (1 << 14);
-		input += probe(1 ,-1,enemies) * (1 << 13);
-		input += probe(-1,0 ,enemies) * (1 << 12);
-		input += probe(1 ,0 ,enemies) * (1 << 11);
-		input += probe(-1,1 ,enemies) * (1 << 10);
-		input += probe(1 ,1 ,enemies) * (1 <<  9);
+    // System.out.println("enemies : "+probe(1,0,enemies));
 
-		/* levelScene情報 */
-		input += probe(-1,-1,levelScene) * (1 << 8);
-		input += probe(0 ,-1,levelScene) * (1 << 7);
-		input += probe(1 ,-1,levelScene) * (1 << 6);
-		input += probe(-1,0 ,levelScene) * (1 << 5);
-		input += probe(1 ,0 ,levelScene) * (1 << 4);
-		input += probe(-1,1 ,levelScene) * (1 << 3);
-		input += probe(1 ,1 ,levelScene) * (1 << 2);
+    /* input から output(act)を決定する */
+    int act = gene[input]; // 遺伝子のinput番目の数値を読み取る
+    for (int i = 0; i < Environment.numberOfKeys; i++) {
+      action[i] = (act % 2 == 1); // 2で割り切れるならtrue
+      act /= 2;
+    }
 
-		input += (isMarioOnGround ? 1: 0) * (1 << 1);
-		input += (isMarioAbleToJump ? 1: 0) * (1 << 0);
+    return action;
+  }
 
-//		System.out.println("enemies : "+probe(1,0,enemies));
+  private double probe(int x, int y, byte[][] scene) {
+    int realX = x + 11;
+    int realY = y + 11;
+    return (scene[realX][realY] != 0) ? 1 : 0;
+  }
 
-		/* input から output(act)を決定する */
-		int act = gene[input];	//遺伝子のinput番目の数値を読み取る
-		for(int i=0; i<Environment.numberOfKeys; i++){
-			action[i] = (act %2 == 1);	//2で割り切れるならtrue
-			act /= 2;
-		}
+  public byte getGene(int i) {
+    return gene[i];
+  }
 
-		return action;
-	}
+  public void setGene(int j, byte gene) {
+    this.gene[j] = gene;
+  }
 
+  public void setDistance(int distance) {
+    this.distance = distance;
+  }
 
+  public int getDistance() {
+    return distance;
+  }
 
-	private double probe(int x, int y, byte[][] scene){
-	    int realX = x + 11;
-	    int realY = y + 11;
-	    return (scene[realX][realY] != 0) ? 1 : 0;
-	}
+  @Override
+  public Evolvable getNewInstance() {
+    // TODO 自動生成されたメソッド・スタブ
+    return null;
+  }
 
-	public byte getGene(int i){
-		return gene[i];
-	}
+  @Override
+  public GAAgent clone() {
 
-	public void setGene(int j,byte gene){
-		this.gene[j] = gene;
-	}
+    GAAgent res = null;
+    try {
+      res = (GAAgent) super.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new InternalError(e.toString());
+    }
 
-	public void setDistance(int distance){
-		this.distance = distance;
-	}
-	public int getDistance(){
-		return distance;
-	}
+    return res;
+  }
 
+  @Override
+  public void mutate() {
+    // TODO 自動生成されたメソッド・スタブ
 
-	@Override
-	public Evolvable getNewInstance() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
+  }
 
-	@Override
-	public GAAgent clone(){
-
-		GAAgent res = null;
-		try{
-			res = (GAAgent)super.clone();
-		}catch(CloneNotSupportedException e){
-			throw new InternalError(e.toString());
-		}
-
-		return res;
-	}
-
-	@Override
-	public void mutate() {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
-	@Override
-	public Evolvable copy() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
+  @Override
+  public Evolvable copy() {
+    // TODO 自動生成されたメソッド・スタブ
+    return null;
+  }
 
 }
--- src/ch/idsia/agents/learning/GAAgent.java
+++ src/ch/idsia/agents/learning/GAAgent.java
@@ -1,17 +1,30 @@
 package ch.idsia.agents.learning;
 
+import java.util.Random;
+
 import ch.idsia.agents.Agent;
 import ch.idsia.agents.controllers.BasicMarioAIAgent;
+import ch.idsia.benchmark.mario.engine.GeneralizerLevelScene;
+import ch.idsia.benchmark.mario.environments.Environment;
 
-public class GAAgent extends BasicMarioAIAgent implements Agent, Comparable {
+public class GAAgent extends BasicMarioAIAgent {
   private static String name = "GAAgent";
   private final int INPUT_NUM = 16;
+  private final int GENE_LENGTH = 1 << INPUT_NUM;
   private int fitness = 0;
-  private int[] gene;
+  private int[] gene = new int[GENE_LENGTH];
+
+  private Random r = new Random();
 
   public GAAgent() {
     super(name);
-    gene = new int[1 << INPUT_NUM];
+    initiateGene();
+  }
+
+  private void initiateGene() {
+    for (int i = 0; i < GENE_LENGTH; i++) {
+      gene[i] = r.nextInt(1 << Environment.numberOfKeys - 1);
+    }
   }
 
   public int getFitness() {
@@ -29,5 +42,61 @@ public class GAAgent extends BasicMarioAIAgent implements Agent, Comparable {
 
   public void setGene(int index, int[] gene) {
     this.gene[index] = gene[index];
+  }
+
+  public boolean[] getAction() {
+    int input = getGeneIndex();
+    int act = gene[input]; // 遺伝子のinput番目の数値を読み取る
+    for (int i = 0; i < Environment.numberOfKeys; i++) {
+      action[i] = (act % 2 == 1); // 2で割り切れるならtrue
+      act /= 2;
+    }
+
+    return action;
+  }
+
+  private int getGeneIndex() {
+    int result = 0;
+
+    /* enemies情報 */
+    result += probe(-1, -1, enemies) * (1 << 15);
+    result += probe(0, -1, enemies) * (1 << 14);
+    result += probe(1, -1, enemies) * (1 << 13);
+    result += probe(-1, 0, enemies) * (1 << 12);
+    result += probe(1, 0, enemies) * (1 << 11);
+    result += probe(-1, 1, enemies) * (1 << 10);
+    result += probe(1, 1, enemies) * (1 << 9);
+
+    /* オブジェクト情報 */
+    result += probe(-1, -1, levelScene) * (1 << 8);
+    result += probe(0, -1, levelScene) * (1 << 7);
+    result += probe(1, -1, levelScene) * (1 << 6);
+    result += probe(-1, 0, levelScene) * (1 << 5);
+    result += probe(1, 0, levelScene) * (1 << 4);
+    result += probe(-1, 1, levelScene) * (1 << 3);
+    result += probe(1, 1, levelScene) * (1 << 2);
+
+    result += (isMarioOnGround ? 1 : 0) * (1 << 1);
+    result += (isMarioAbleToJump ? 1 : 0) * (1 << 0);
+    return result;
+  }
+
+  private double probe(int x, int y, byte[][] scene) {
+    int realX = x + 11;
+    int realY = y + 11;
+    return (scene[realX][realY] != 0) ? 1 : 0;
+  }
+
+  @Override
+  public GAAgent clone() {
+
+    GAAgent res = null;
+    try {
+      res = (GAAgent) super.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new InternalError(e.toString());
+    }
+
+    return res;
   }
 }
